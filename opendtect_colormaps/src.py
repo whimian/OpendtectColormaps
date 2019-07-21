a8e94c9f675688632b6078766eacb3d102839ccd
Yu Hao <yuhao89@live.cn>
2019-07-21 20:16:15 +0800

    add support for segmented colormaps

--- opendtect_colormaps/src.py
+++ opendtect_colormaps/src.py
@@ -25,18 +25,24 @@ class OpendtectColormaps(object):
         return list(self.color_dicts.keys())
 
     def __call__(self, cm):
-        return LinearSegmentedColormap.from_list(
+        n_levels = self.color_dicts[cm]['N']
+        n_levels = 256 if (n_levels is None or n_levels == 0) else n_levels
+        cmap = LinearSegmentedColormap.from_list(
             cm,
-            colors=self.color_dicts[cm],
-            N=256,
+            colors=self.color_dicts[cm]['cdict'],
+            N=n_levels,
             gamma=1.0)
+        undef_color = self.color_dicts[cm]['undef']
+        if undef_color is not None:
+            cmap.set_bad(undef_color)
+        return cmap
 
     def _convert_to_cm(self):
         with open(self.od_cm_file, 'r') as fl:
             text = fl.readlines()
 
         text_select = []
-        num=-1
+        num = -1
         for line in text:
             if line.split('.')[0].isdigit():
                 num_color = int(line.split('.')[0])-1
@@ -49,18 +55,25 @@ class OpendtectColormaps(object):
 
         color_dicts = {}
         for dic in text_select:
-            color_dicts[self.get_name(dic)] = self.get_cdict(dic)
-
+            color_dicts[self.get_name(dic)] = {
+                "cdict": self.get_cdict(dic),
+                "undef": self.get_undef_color(dic),
+                "N": self.get_nr_segments(dic)
+            }
         self.color_dicts = color_dicts
 
-    def get_name(self, abc):
+    @staticmethod
+    def get_name(abc):
+        colormap_name = None
         for k in abc.keys():
             if "Name" in k:
-                return abc[k].rstrip('\n').lstrip(' ').\
+                colormap_name = abc[k].rstrip('\n').lstrip(' ').\
                     replace('-', '_').replace(' ', '_')
+                break
+        return colormap_name
 
     def get_cdict(self, abc):
-        cdict=[]
+        cdict = []
         for k in abc.keys():
             if "Value-Color" in k:
                 str_list = abc[k].rstrip("\n").split('`')
@@ -69,3 +82,23 @@ class OpendtectColormaps(object):
                 list_int = [self.normalizer(int(i)) for i in str_list[1:]]
                 cdict.append(tuple((v, tuple(list_int))))
         return cdict
+
+    def get_undef_color(self, abc):
+        undef_color = None
+        for k in abc.keys():
+            if "Undef color" in k:
+                str_list = abc[k].rstrip("\n").split('`')
+                str_list = str_list[:-1] if len(str_list) == 4 else str_list
+                list_int = [self.normalizer(int(i)) for i in str_list]
+                undef_color = tuple(list_int)
+                break
+        return undef_color
+
+    @staticmethod
+    def get_nr_segments(abc):
+        nr_segments = None
+        for k in abc.keys():
+            if "Nr segments" in k:
+                nr_segments = int(abc[k].rstrip("\n"))
+                break
+        return nr_segments
